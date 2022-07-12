94ea59e9ccab0bd1fc32efcb58f4e0ddb0026207
RonaldBorda <ronald.borda@tecsup.edu.pe>
2022-07-12 13:52:56 -0500

    actualizaciones

--- APIBitcoin/View/ViewController.swift
+++ APIBitcoin/View/ViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     let bitcoinViewModel: BitcoinViewModel = BitcoinViewModel()
+    var filterData: [Rate] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,31 +25,39 @@ class ViewController: UIViewController {
     }
     func setUpData() async{
         await bitcoinViewModel.getDataFromAPI()
+        filterData = bitcoinViewModel.bitcoins
         print(bitcoinViewModel.bitcoins)
         tableView.reloadData()
     }
     func setUpView(){
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
     }
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return bitcoinViewModel.bitcoins.count
+        return filterData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-            let bitcoin = bitcoinViewModel.bitcoins[indexPath.row]
+            let bitcoin = filterData[indexPath.row]
             cell.textLabel?.text = bitcoin.name
             return cell
     }
     
-    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//    Dictionary<String, Rate>.Index
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-//        cell.textLabel?.text = pokeViewModel.pokemons[indexPath.row].name.capitalized
-//        return cell
-//    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(filterData[indexPath.row].name)
+    }
+}
+extension ViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterData = searchText.isEmpty
+        ? bitcoinViewModel.bitcoins
+        : bitcoinViewModel.bitcoins.filter({ (bitcoin: Rate) -> Bool in
+            return bitcoin.name.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        })
+        tableView.reloadData()
+    }
 }
